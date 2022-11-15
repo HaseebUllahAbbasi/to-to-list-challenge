8f53dddd10c21887f8ef22866abb7d0d3b33df33
haseebullahabbasi <haseebabbasi00@gmail.com>
2022-11-16 03:44:26 +0500

    Task component added

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,53 +1,10 @@
 import React, { FC } from "react"
-import { DraggableProvided, DraggableStateSnapshot, DropResult } from "react-beautiful-dnd"
+import { DraggableProvided, DraggableStateSnapshot } from "react-beautiful-dnd"
 import Swal from 'sweetalert2'
 
 import { TaskItem, Columns } from "../types"
 
 
-/**
- *  Handling the Dragging Event and checking valid point to be dragged
- *   then Changing Data Accordingly
- * **/
-const onDragEnd = (
-  result: DropResult,
-  data: Columns[],
-  setColumns: {
-    (value: React.SetStateAction<Columns[]>): void;
-    (arg0: any[]): void;
-  }
-) => {
-
-  if (!result.destination) return;
-  const { source, destination } = result;
-  if (source.droppableId !== destination.droppableId) {
-    const sourceId = source.droppableId;
-    const destinationId = destination.droppableId;
-    const Pickedindex = source.index;
-    const DroppedIndex = destination.index;
-    const SourceIndex = data.findIndex(
-      (item: { id: any }) => item.id === sourceId
-    );
-    const DestIndex = data.findIndex(
-      (item: { id: any }) => item.id === destinationId
-    );
-    const NewItem = data[SourceIndex].items.splice(Pickedindex, 1);
-    data[DestIndex].items.splice(DroppedIndex, 0, NewItem[0]);
-    setColumns([...data]);
-  }
-  else {
-    const colId = source.droppableId;
-    const Pickedindex = source.index;
-    const SourceIndex = data.findIndex(
-      (item: { id: any }) => item.id === colId
-    );
-    const removed = data[SourceIndex].items.splice(Pickedindex, 1);
-    data[SourceIndex].items.splice(destination.index, 0, removed[0]);
-    setColumns([...data])
-
-  }
-
-};
 /**
  * Deleting the Selected Task
  * */
@@ -89,9 +46,10 @@ const onComplete = (
   setColumns([...data]);
 };
 
+
 type TaskProps = {
   item: TaskItem,
-  taskText: string,
+  searchText: string,
   provided: DraggableProvided,
   snapshot: DraggableStateSnapshot,
   index: number,
@@ -104,12 +62,12 @@ type TaskProps = {
   ColIndex: number,
 }
 
-const Task: FC<TaskProps> = ({ item, taskText, provided, snapshot, index, setColumns, columns, colItem, ColIndex }) => {
+const Task: FC<TaskProps> = ({ item, searchText, provided, snapshot, index, setColumns, columns, colItem, ColIndex }) => {
 
   return (
 
     <div
-      hidden={!item.text.includes(taskText)}
+      hidden={!item.text.includes(searchText)}
       className="card mx-2 mb-3 mt-2"
       ref={provided.innerRef}
       {...provided.draggableProps}
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
+import Task from "./Task";
 
 /**
  *  Handling the Dragging Event and checking valid point to be dragged
@@ -53,47 +54,12 @@ const onDragEnd = (
  * Deleting the Selected Task
  * */
 
-const onDelete = (
-  Pickedindex: number,
-  data: Columns[],
-  setColumns: {
-    (value: React.SetStateAction<Columns[]>): void;
-    (arg0: any[]): void;
-  },
-  ColName: string,
-  ColIndex: number
-) => {
-  data[ColIndex].items.splice(Pickedindex, 1);
-  setColumns([...data]);
-};
 
-/**
- * Completing the Task  if not  Completed
- * 
- * */
-const onComplete = (
-  Pickedindex: number,
-  data: { items: any[] }[],
-  setColumns: {
-    (value: React.SetStateAction<Columns[]>): void;
-    (arg0: any[]): void;
-  },
-  ColName: string,
-  ColIndex: number
-) => {
-  if (ColName === "Completed") {
-    alert("Already Completed ");
-    return;
-  }
-  const NewItem = data[ColIndex].items.splice(Pickedindex, 1);
-  data[1].items.splice(0, 0, NewItem[0]);
-  setColumns([...data]);
-};
 
 const Tasks: FC = () => {
   const MySwal = withReactContent(Swal);
   const [columns, setColumns] = useState<Columns[]>([...columsData]);
-  const [taskText, setTaskText] = useState<string>("");
+  const [searchText, setTaskText] = useState<string>("");
 
 
 
@@ -109,7 +75,7 @@ const Tasks: FC = () => {
             className="form-control"
             // style={{ width: "" }}
 
-            value={taskText}
+            value={searchText}
             placeholder={"Search"}
             onChange={(e) => setTaskText(e.target.value)}
           />
@@ -205,133 +171,7 @@ const Tasks: FC = () => {
                               {(provided, snapshot) => {
                                 return (
 
-                                  <div
-                                    hidden={!item.text.includes(taskText)}
-                                    className="card mx-2 mb-3 mt-2"
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-
-                                    style={{
-                                      userSelect: "none",
-                                      paddingTop: "10px",
-                                      borderRadius: "10px",
-                                      margin: "0 0 8px 0",
-                                      minHeight: "50px",
-                                      minWidth: "120px",
-
-
-                                      backgroundColor: snapshot.isDragging
-                                        ? "#F5E0B7"
-                                        : "#E8EDDF",
-                                      color: "black",
-                                      ...provided.draggableProps.style
-                                    }}
-                                  >
-                                    <div
-                                      className="card-body display-6 text-center"
-                                      style={{ padding: 10, minHeight: "110px", fontSize: "2rem", }}
-                                    >
-                                      {item.text
-                                      }
-                                    </div>
-                                    <div className="card-footer d-flex  justify-content-around   flex-wrap">
-                                      <button
-                                        title="Delete"
-                                        className="btn d-block btn-danger"
-                                        onClick={() => {
-                                          Swal.fire({
-                                            title: 'Are you sure?',
-                                            text: "do you  want to delete !",
-                                            icon: 'warning',
-                                            showCancelButton: true,
-                                            confirmButtonColor: '#d33',
-                                            cancelButtonColor: '#3085d6 ',
-                                            confirmButtonText: 'Yes, delete it!'
-                                          }).then((result) => {
-                                            if (result.isConfirmed) {
-
-                                              onDelete(
-                                                index,
-                                                columns,
-                                                setColumns,
-                                                colItem.name,
-                                                ColIndex
-                                              );
-                                              Swal.fire(
-                                                'Deleted!',
-                                                'Task Deleted',
-                                                'success'
-                                              )
-                                            }
-                                          })
-                                        }}
-                                      >
-                                        <span role="img" aria-label="Remove">
-                                          ❌
-                                        </span>
-                                      </button>
-                                      <button
-                                        title="Edit Task"
-                                        className="btn btn-warning"
-                                        onClick={() => {
-
-                                          MySwal.fire(
-                                            {
-                                              title: 'Modify Task 📝',
-                                              input: 'text',
-                                              inputValue: item.text,
-                                              inputAttributes:
-                                              {
-                                                autocapitalize: 'off'
-                                              },
-                                              showCancelButton: true,
-                                              confirmButtonText: 'Edit',
-                                              showLoaderOnConfirm: true,
-                                              allowOutsideClick: () => !Swal.isLoading()
-                                            }
-                                          )
-                                            .then((result) => {
-                                              if (result.isConfirmed) {
-                                                const data = [...columns];
-                                                data[ColIndex].items[index].text = result.value;
-                                                setColumns([...data]);
-                                                Swal.fire({
-                                                  title: `Task Modified`,
-                                                  text: (result.value),
-                                                })
-
-                                              }
-                                            })
-
-
-                                        }}
-                                      >
-                                        <span role="img" aria-label="Edit">
-                                          📝
-                                        </span>
-                                      </button>
-                                      {
-                                        colItem.name !== "Completed" &&
-                                        <button
-                                          className="btn btn-success"
-                                          title="Complete"
-                                          onClick={() => {
-                                            onComplete(
-                                              index,
-                                              columns,
-                                              setColumns,
-                                              colItem.name,
-                                              ColIndex
-                                            );
-                                          }}
-                                        >
-                                          ✔
-                                        </button>
-
-                                      }
-                                    </div>
-                                  </div>
+                                  <Task columns={columns} index={index} provided={provided} snapshot={snapshot} item={item} setColumns={setColumns} searchText={searchText} colItem={colItem} ColIndex={ColIndex} />
 
                                 );
                               }}
